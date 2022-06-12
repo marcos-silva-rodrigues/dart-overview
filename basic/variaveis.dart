a8d7b80216272646b0e5096af7f9054bc0aebccc
marcos-silva-rodrigues <marcos.rodrigues.dev@gmail.com>
2022-06-12 14:47:50 -0300

    feat: variaveis primitivas

---
+++ basic/variaveis.dart
@@ -0,0 +1,15 @@
+
+void main() {
+  String nome = 'Fulano';
+  nome = 'Ciclano';
+  print(nome);
+
+  int idade = 33;
+  print(idade);
+
+  double peso = 69.5;
+  print(peso);
+
+  bool maiorDeIdade = idade >= 18;
+  print(maiorDeIdade);
+}
