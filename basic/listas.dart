3a449f0fbe31c6674828f1e87f41e43fc13165e3
marcos-silva-rodrigues <marcos.rodrigues.dev@gmail.com>
2022-06-12 15:00:32 -0300

    feat: listas

---
+++ basic/listas.dart
@@ -0,0 +1,24 @@
+
+void main() {
+  List<int> listaDeCompras = [
+    1, 2, 3, 4, 5, 6, 7, 8, 9, 10
+  ];
+  print(listaDeCompras.length);
+
+  List user = [
+    'Erik',
+    33,
+    75.5,
+    true
+  ];
+  print(user);
+
+  Map userTwo = {
+    'nome': 'Marcos',
+    'idade': 33,
+    'peso': 75.5,
+    'maiorDeIdade': true,
+  };
+
+  print(userTwo);
+}
